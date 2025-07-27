d718da436a1a3cda0f5b9db6468de55b1bbf1e75
Made By IToncek <itoncek3@itoncek.space>
2025-07-27 14:27:24 +0200

    Adding CI

---
+++ ci/install-godot.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ ! -d /tmp/engine-godot ]; then
+    sudo apt-get install jq unzip -y
+
+    curl https://api.github.com/repos/godotengine/godot/releases/latest > /tmp/engine-ver
+
+    cd /tmp/
+    url=$(cat /tmp/engine-ver|jq '.assets[] | select (.name | endswith("_mono_linux_x86_64.zip"))' | jq '.browser_download_url')
+
+    echo "$url"
+    echo "$url"|xargs wget -O "godot.zip" -c {}
+
+    unzip /tmp/godot.zip
+    mv /tmp/Godot_* /tmp/engine-godot
+
+    rm /tmp/engine-ver
+    rm /tmp/godot.zip
+fi
+
+if [ ! -d /tmp/templates.tpz ]; then
+    sudo apt-get install jq unzip -y
+
+    curl https://api.github.com/repos/godotengine/godot/releases/latest > /tmp/templates-ver
+
+    cd /tmp/
+    url=$(cat /tmp/templates-ver|jq '.assets[] | select (.name | endswith("_mono_export_templates.tpz"))' | jq '.browser_download_url')
+
+    echo "$url"
+    echo "$url"|xargs wget -O "templates.tpz" -c {}
+
+fi
+
