d603673d208f2d383dd213d092fdc9a7d1c86715
Made By IToncek <itoncek3@itoncek.space>
2025-07-27 15:17:44 +0200

    moving to runner's temp information

--- ci/install-godot.sh
+++ ci/install-godot.sh
@@ -2,38 +2,38 @@
 
 sudo apt-get install jq unzip -y
 
-if [ ! -d /tmp/engine-godot ]; then
+if [ ! -d /$RUNNER_TEMP/engine-godot ]; then
 
-    curl https://api.github.com/repos/godotengine/godot/releases/latest > /tmp/engine-ver
+    curl https://api.github.com/repos/godotengine/godot/releases/latest > /$RUNNER_TEMP/engine-ver
 
-    cd /tmp/
-    url=$(cat /tmp/engine-ver|jq '.assets[] | select (.name | endswith("_mono_linux_x86_64.zip"))' | jq '.browser_download_url')
+    cd /$RUNNER_TEMP/
+    url=$(cat /$RUNNER_TEMP/engine-ver|jq '.assets[] | select (.name | endswith("_mono_linux_x86_64.zip"))' | jq '.browser_download_url')
 
     echo "$url"
     echo "$url"|xargs wget -q -O "godot.zip" -c {}
 
-    unzip /tmp/godot.zip
-    mv /tmp/Godot_* /tmp/engine-godot
-    mv /tmp/engine-godot/Godot_* /tmp/engine-godot/godot
+    unzip /$RUNNER_TEMP/godot.zip
+    mv /$RUNNER_TEMP/Godot_* /$RUNNER_TEMP/engine-godot
+    mv /$RUNNER_TEMP/engine-godot/Godot_* /$RUNNER_TEMP/engine-godot/godot
 
-    rm /tmp/engine-ver
-    rm /tmp/godot.zip
+    rm /$RUNNER_TEMP/engine-ver
+    rm /$RUNNER_TEMP/godot.zip
 fi
 
-if [ ! -d /tmp/templates ]; then
+if [ ! -d /$RUNNER_TEMP/templates ]; then
 
-    curl https://api.github.com/repos/godotengine/godot/releases/latest > /tmp/templates-ver
+    curl https://api.github.com/repos/godotengine/godot/releases/latest > /$RUNNER_TEMP/templates-ver
 
-    cd /tmp/
-    url=$(cat /tmp/templates-ver|jq '.assets[] | select (.name | endswith("_mono_export_templates.tpz"))' | jq '.browser_download_url')
+    cd /$RUNNER_TEMP/
+    url=$(cat /$RUNNER_TEMP/templates-ver|jq '.assets[] | select (.name | endswith("_mono_export_templates.tpz"))' | jq '.browser_download_url')
 
     echo "$url"
     echo "$url"|xargs wget -q -O "templates.tpz" -c {}
 
-    unzip /tmp/templates.tpz
+    unzip /$RUNNER_TEMP/templates.tpz
     
-    rm /tmp/templates-ver
-    rm /tmp/templates.tpz
+    rm /$RUNNER_TEMP/templates-ver
+    rm /$RUNNER_TEMP/templates.tpz
 fi
 
 exit 0
